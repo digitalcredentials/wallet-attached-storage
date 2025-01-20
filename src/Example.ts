2c76ca89caa938576d917fffe3af87e32953486b
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-01-20 21:11:53 +0000

    Initial commit

---
+++ src/Example.ts
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2025 Digital Credentials Consortium. All rights reserved.
+ */
+export class Example {
+  public hello (): string {
+    return 'world'
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/*!
+ * Copyright (c) 2025 Digital Credentials Consortium. All rights reserved.
+ */
+export { Example } from './Example'
