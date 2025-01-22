8b98ec14bb59e61d04e8d30110c3068de37be831
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-01-21 22:13:04 -0500

    Sketch provisionSpace().

--- src/WalletStorage.ts
+++ src/WalletStorage.ts
@@ -1,8 +1,33 @@
 /*!
  * Copyright (c) 2025 DID.coop. All rights reserved.
  */
+import { DataPubFetchClient } from '@data.pub/fetch-client'
+import { ISpace } from '@data.pub/fetch-client/lib/types'
+
+export interface ISigner {
+  id?: string
+  sign: (signable: { data: Uint8Array }) => Promise<Uint8Array>
+}
+
 export class WalletStorage {
-  public hello (): string {
-    return 'world'
+  // static connect ({ url, signer }: { url: string, signer: ISigner }): ? {
+  // }
+
+  /**
+   * Provisions an ephemeral Wallet Attached Storage space (for use by an
+   * application, to receive zcaps etc).
+   *
+   * @param url {string} - URL to a DataPub server.
+   * @param signer {ISigner} - Ed25519 did:key Signer.
+   */
+  static async provisionSpace (
+    { url, signer }: { url: string | URL, signer: ISigner }
+  ): Promise<ISpace> {
+    const pub = new DataPubFetchClient(new URL(url))
+
+    const space = pub.space({ signer })
+    // Create a new space
+    const spaceResponse = await space.put()
+
   }
 }
